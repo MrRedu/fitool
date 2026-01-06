60ee46e932f54cb4236bfb4329eb36e818f136b3
Eduardo <ejrb1234@gmail.com>
2026-01-06 02:55:56 -0400

    feat(layout): Enhance metadata for improved SEO and user engagement, including title, description, keywords, authors, and Open Graph settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,52 @@ const geistMono = Geist_Mono({
 })
 
 export const metadata: Metadata = {
-  title: 'FiTool',
-  description: '',
+  metadataBase: new URL('https://fitool.vercel.app/'),
+  title: {
+    default: 'FiTool',
+    template: '%s | FiTool',
+  },
+  description:
+    'FiTool es el gimnasio de más alto rendimiento con planes guiados, equipamiento profesional y calculadoras para optimizar entrenamiento, nutrición y recuperación.',
+  keywords: [
+    'gimnasio',
+    'fitness',
+    'entrenamiento',
+    'nutrición',
+    'salud',
+    'calculadoras fitness',
+    'planes de entrenamiento',
+  ],
+  authors: [
+    { name: '(Mr Redu) - Eduardo R.', url: 'https://github.com/MrRedu' },
+  ],
+  alternates: {
+    canonical: '/',
+  },
+  openGraph: {
+    title: 'FiTool | Gimnasio de alto rendimiento',
+    description:
+      'Entrena en FiTool con planes personalizados, equipo pro y calculadoras para macronutrientes y gasto energético. Lleva tu rendimiento al siguiente nivel con un gimnasio pensado para progresar.',
+    url: '/',
+    siteName: 'FiTool',
+    locale: 'es_ES',
+    type: 'website',
+    images: [
+      {
+        url: '/cover-page.webp',
+        width: 800,
+        height: 600,
+        alt: 'FiTool | Gimnasio de alto rendimiento',
+      },
+      {
+        url: '/cover-page.webp',
+        width: 1800,
+        height: 1600,
+        alt: 'FiTool | Gimnasio de alto rendimiento',
+      },
+    ],
+  },
+  category: 'health & fitness',
 }
 
 export default function RootLayout({
